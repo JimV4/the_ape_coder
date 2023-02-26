259ada1e4c8e23deea86a2a3c6cb5c33ab1a7f8b
Dimitris Vassiliou <dhmhtrhs.vassiliou@gmail.com>
2023-02-26 13:36:28 +0200

    added email to all languages

--- lib/Java/JavaTest.dart
+++ lib/Java/JavaTest.dart
@@ -4,6 +4,9 @@ import 'package:ape_coder/Java/JavaTestSuccessful.dart';
 import 'JavaLessons.dart';
 import 'package:vibration/vibration.dart';
 import 'package:audioplayers/audioplayers.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:ape_coder/Profile.dart';
 
 const backgroundcolor = Color.fromARGB(255, 47, 47, 47);
 bool Q1A_checked_java = false;
@@ -36,6 +39,43 @@ class JavaTestWidget extends StatefulWidget {
 }
 
 class _JavaTestWidgetState extends State<JavaTestWidget> {
+  Future sendEmail() async {
+    const serviceId = 'service_1gr82kg';
+    const templateId = 'template_2lfcu1c';
+    const publicKey = 'I3cCCV2WgXSIetL5W';
+    const privateKey = 'RP9ig4sXWnF2RQaFX0QPF';
+    const language = 'Java';
+
+    final url = Uri.parse('https://api.emailjs.com/api/v1.0/email/send');
+    final response = await http.post(
+      url,
+      headers: {
+        'origin': 'http://localhost',
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({
+        'service_id': serviceId,
+        'template_id': templateId,
+        'user_id': publicKey,
+        'accessToken': privateKey,
+        'template_params': {
+          'user_name': textController.text.toString(),
+          'user_email': textController2.text.toString(),
+          'user_subject': 'Your certificate from The-Ape-Coder!',
+          'user_message':
+              'Congratulations! You have successfully completed the $language test!. This is your certificate!'
+          /* ' This email shows that you are worthy of holding the holly programming banana!'
+              ' Farewell as you continue your journey in the jungle of programming languages!' */
+          ,
+          'from_email': 'dhmhtrhs.vassiliou@gmail.com'
+        }
+      }),
+    );
+    debugPrint('here');
+    debugPrint(response.body);
+    debugPrint(textController2.text.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
     return (Scaffold(
